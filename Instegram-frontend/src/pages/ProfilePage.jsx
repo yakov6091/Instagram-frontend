3f6ccc66d1378bba38c5b228eb6273a96c67bf5a
Yakov <yakov6091@gmail.com>
2025-10-03 08:02:56 +0300

    Initial profile

---
+++ Instegram-frontend/src/pages/ProfilePage.jsx
@@ -0,0 +1,15 @@
+import { Profile } from "../cmps/Profile"
+import { useState } from "react"
+
+export function ProfilePage() {
+
+    return (
+        <section className="profile-container">
+            <Profile />
+
+        </section>
+    )
+
+
+
+}
